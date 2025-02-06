253768fb073eeae453bfe0890e8d9932c377c644
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-06 21:56:18 +0100

    renamed this project

---
+++ LiezVosChampsDeSaisieAvecVosTablesSansLBSousFMX/LinkDBFieldsWithTables.dpr
@@ -0,0 +1,46 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Developpeur Pascal Blog Sample Projects
+///
+/// Copyright 2015-2025 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/exemples
+///
+/// ***************************************************************************
+/// File last update : 2025-02-06T21:36:18.000+01:00
+/// Signature : 4209dd48253ec9bbe1c432803d3f8b5210b83fb4
+/// ***************************************************************************
+/// </summary>
+
+program LinkDBFieldsWithTables;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Unit1 in 'Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
