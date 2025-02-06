0d2d9ad88bf442c1000d424e829c36ae74ef2916
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-06 21:57:18 +0100

    renamed this project

---
+++ simplifierLePassageDeParametreDeListes/UsingListsAsParameters3.dpr
@@ -0,0 +1,46 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Developpeur Pascal Blog Sample Projects
+///
+/// Copyright 2015-2025 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/exemples
+///
+/// ***************************************************************************
+/// File last update : 2025-02-06T21:38:44.000+01:00
+/// Signature : feccd4bc04cc4666c05a12a708a32b66566a868e
+/// ***************************************************************************
+/// </summary>
+
+program UsingListsAsParameters3;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Unit3 in 'Unit3.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
