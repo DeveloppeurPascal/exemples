ea472bbe9ee061ca0792e8500e87fb4690856795
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-06 21:58:18 +0100

    renamed this project

---
+++ utiliseruneunitecommunedansunprojetvcletfmx/console/ShareAUnitWithAConsoleProject.dpr
@@ -0,0 +1,52 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Developpeur Pascal Blog Sample Projects
+///
+/// Copyright 2015-2025 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/exemples
+///
+/// ***************************************************************************
+/// File last update : 2025-02-06T21:42:06.000+01:00
+/// Signature : 2c6baf0a3020b869d785832b88cb68438a5a2646
+/// ***************************************************************************
+/// </summary>
+
+program ShareAUnitWithAConsoleProject;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses
+  System.SysUtils,
+  CommonUnit in '..\CommonUnit.pas';
+
+begin
+  try
+    AfficheMessage('Hello World');
+
+    while true do;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+
+end.
