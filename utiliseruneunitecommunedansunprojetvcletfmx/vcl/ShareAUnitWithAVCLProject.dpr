6c1e9aad12516a78d79f090904ff5eac999be29e
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-06 21:58:25 +0100

    renamed this project

---
+++ utiliseruneunitecommunedansunprojetvcletfmx/vcl/ShareAUnitWithAVCLProject.dpr
@@ -0,0 +1,47 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Developpeur Pascal Blog Sample Projects
+///
+/// Copyright 2015-2025 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/exemples
+///
+/// ***************************************************************************
+/// File last update : 2025-02-06T21:43:24.000+01:00
+/// Signature : bee9c143f216e4acd02b15f89106a6cf5af41d99
+/// ***************************************************************************
+/// </summary>
+
+program ShareAUnitWithAVCLProject;
+
+uses
+  Vcl.Forms,
+  Unit2 in 'Unit2.pas' {Form2},
+  CommonUnit in '..\CommonUnit.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
